914f4b5fec4b8039dcae3efc18efc32d57421353
Henry Zheng <henry.zheng727@gmail.com>
2026-05-19 12:35:18 -0600

    problem 2540

---
+++ 2540.py
@@ -0,0 +1,52 @@
+# @leet imports start
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+import string
+import re
+import datetime
+import collections
+import heapq
+import bisect
+import copy
+import math
+import random
+import statistics
+import itertools
+import functools
+import operator
+import io
+import sys
+import json
+from typing import *
+# @leet imports end
+
+# @leet start
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        i = 0
+        j = 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            if nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        return -1
+
+# @leet end
